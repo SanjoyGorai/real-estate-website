aee49ef9bad6c1ed330b32cadfeff528c3bdee4d
Mr. Sanjoy <--add>
2024-01-03 18:08:52 +0530

    slider and slider buttons are working

--- src/componants/Residencies/Residencies.jsx
+++ src/componants/Residencies/Residencies.jsx
@@ -15,6 +15,7 @@ const Residencies = () => {
                 </div>
 
                 <Swiper {...sliderSettings} >
+                    <SliderButtons />
                     {
                         data.map((card, i) => (
                             <SwiperSlide key={i}>
@@ -37,3 +38,13 @@ const Residencies = () => {
 };
 
 export default Residencies
+
+const SliderButtons = () => {
+    const swiper = useSwiper();
+    return (
+        <div className="flexCenter r-buttons">
+            <button onClick={() => swiper.slidePrev()}>&lt;</button>
+            <button onClick={() => swiper.slideNext()}>&gt;</button>
+        </div>
+    )
+}
